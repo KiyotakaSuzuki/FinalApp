18839b81e7a4d80b1bfa56195188d85a57b5d181
kyoto2iko <110676289+KiyotakaSuzuki@users.noreply.github.com>
2022-09-22 13:08:08 +0900

    Update model.py

--- model.py
+++ model.py
@@ -33,12 +33,12 @@ with open("imagenet_classes.txt") as f:
 #前処理（トランスフォーム作成）
 def predict(img):
     transform = transforms.Compose([
-                                    transforms.Resize(256),
-                                    transforms.CenterCrop(224),
+                                    transforms.Resize(480),
+                                    transforms.CenterCrop(480),
                                     transforms.ToTensor(),
                                     transforms.Normalize(
-                                        mean=[0.485, 0.456, 0.406],
-                                        std=[0.229, 0.224, 0.225]
+                                        mean=[0.5, 0.5, 0.5],
+                                        std=[0.5, 0.5, 0.5]
                                         )
                                     ])
 
